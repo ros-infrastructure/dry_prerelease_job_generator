5ffa636d44eeb5e3984be64b5fd2469aaf3bda27
Tully Foote <tfoote@willowgarage.com>
2010-04-07 21:53:32 +0000

    reving revision

--- rosinstall/setup.py
+++ rosinstall/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='rosinstall',
-      version='0.01',
+      version='0.02',
       packages=['rosinstall', 'rosinstall.vcs'],
       package_dir = {'':'src'},
       scripts = ["scripts/rosinstall"],
