db0547058323e7224e05d4425ac19a551864dd89
Tully Foote <tfoote@willowgarage.com>
2010-05-12 19:59:32 +0000

    fixing ros package path in setup.sh and reving release

--- rosinstall/setup.py
+++ rosinstall/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='rosinstall',
-      version='0.04.3',
+      version='0.04.4',
       packages=['rosinstall', 'rosinstall.vcs'],
       package_dir = {'':'src'},
       scripts = ["scripts/rosinstall"],
