1d44ae304f771e4bfceeec05f9892a80491fe99c
Ken Conley <kwc@kwc.org>
2010-06-07 22:12:09 +0000

    alternative version to resync previously copied tags

--- release/scripts/copy_distro.py
+++ release/scripts/copy_distro.py
@@ -39,9 +39,12 @@ def main():
         cmds = []
         # for every stack in from, copy to the new distro
         for stack_name, stack in from_distro.stacks.iteritems():
+            #if not stack_name in ['navigation', 'navigation_experimental', 'pr2_ethercat_drivers', 'pr2_robot', 'vision_opencv', 'web_interface']:
+            #    continue
             from_uri = stack.distro_svn
             to_uri = to_distro.stacks[stack_name].distro_svn
             if not release.svn_url_exists(to_uri):
+                #cmds.append(['svn', 'rm', '-m', "resyncing from latest", to_uri]) 
                 cmds.append(['svn', 'cp', '-m', 'copying from %s to %s'%(from_release_name, to_release_name), from_uri, to_uri]) 
         
         if not hasattr(roslib.scriptutil, 'ask_and_call'):
