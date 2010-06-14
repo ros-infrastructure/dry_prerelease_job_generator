e298b5540a56d739f1118c18757b118bc53edd02
Tully Foote <tfoote@willowgarage.com>
2010-06-14 22:02:29 +0000

    release number to 0.04.6

--- rosinstall/setup.py
+++ rosinstall/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='rosinstall',
-      version='0.04.5',
+      version='0.04.6',
       packages=['rosinstall', 'rosinstall.vcs'],
       package_dir = {'':'src'},
       scripts = ["scripts/rosinstall"],
