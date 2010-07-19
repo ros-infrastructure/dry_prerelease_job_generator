020b95f4757b1e34268fcc7acba1721ab354da06
Ken Conley <kwc@kwc.org>
2010-07-19 21:51:10 +0000

    support for auto-generated rosdoc URI

--- release/scripts/externals.py
+++ release/scripts/externals.py
@@ -18,13 +18,18 @@ print "WARNING: this script requires ROS 1.1+"
 def main():
     try:
         from optparse import OptionParser
-        parser = OptionParser(usage="usage: %prog <release>", prog=NAME)
+        parser = OptionParser(usage="usage: %prog <release>\n\tUse 'rosdoc' as the release name to general rosdoc external", prog=NAME)
         options, args = parser.parse_args()
         if len(args) != 1:
             parser.error("""You must specify: 
  * release name""")
         release_name = args[0]
 
+        rosdoc_external = release_name == 'rosdoc'
+        if rosdoc_external:
+            # general based on the cturtle list of stacks
+            release_name = 'cturtle'
+            
         distro = Distro("http://ros.org/distros/%s.rosdistro"%release_name)
 
         # create text for externals
@@ -43,19 +48,33 @@ def main():
             externals.append((name, svn_external))
 
         # create an all external for everything
+        #  - also create the rosdoc external just in case we need it
         all = ''
+        rosdoc_all = ''
         for stack_name, stack in distro.stacks.iteritems():
             if stack_name == 'ros':
                 continue
             uri = stack.distro_svn
             all += tmpl%locals()
+            
+            uri = stack.dev_svn
+            rosdoc_all += tmpl%locals()
+            
         externals.append(('all', all))
         
         if not hasattr(roslib.scriptutil, 'ask_and_call'):
             print >> sys.stderr, "this script only works with ROS 1.1+"
             sys.exit(1)
 
-        external_uri = "https://code.ros.org/svn/wg-ros-pkg/externals/distro/%(release_name)s/%(name)s"
+        if rosdoc_external:
+            external_uri = "https://code.ros.org/svn/wg-ros-pkg/externals/rosdoc"
+            rosdoc_all += """ros-pkg-trunk https://code.ros.org/svn/ros-pkg/trunk
+wg-ros-pkg-trunk https://code.ros.org/svn/wg-ros-pkg/trunk
+"""
+            externals = (('rosdoc', rosdoc_all),)
+        else:
+            external_uri = "https://code.ros.org/svn/wg-ros-pkg/externals/distro/%(release_name)s/%(name)s"
+            
         svn = SVNClient('.')
         for name, svn_external in externals:
             cmds = []
