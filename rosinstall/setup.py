9eb6c80184d7d1ed1cb4384c17bec22be41b78e8
Tully Foote <tfoote@willowgarage.com>
2010-08-16 20:45:51 +0000

    version 0.5.6 and removing debugging statement

--- rosinstall/setup.py
+++ rosinstall/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='rosinstall',
-      version='0.5.5',
+      version='0.5.6',
       packages=['rosinstall', 'rosinstall.vcs'],
       package_dir = {'':'src'},
       scripts = ["scripts/rosinstall"],
