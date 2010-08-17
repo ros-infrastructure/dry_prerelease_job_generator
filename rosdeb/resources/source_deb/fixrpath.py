38ec6ff6a367740d03f1ec149f7700f4bb9384b7
Jeremy Leibs <jleibs@gmail.com>
2010-08-17 23:09:39 +0000

    Adding script to fix pc files

--- rosdeb/resources/source_deb/fixrpath.py
+++ rosdeb/resources/source_deb/fixrpath.py
@@ -13,17 +13,16 @@ def main(argv, stdout, environ):
   parser = OptionParser(__doc__.strip())
   parser.add_option("-t","--test",action="store_true", dest="test",default=False,
                     help="A testing flag")
-  parser.add_option("--path",action="store",dest="path",default=".")
-#  parser.add_option("-v","--var",action="store",type="string", dest="var",default="blah")
 
   (options, args) = parser.parse_args()
 
-  if (len(args) < 2):
+  if (len(args) < 3):
     print >> sys.stderr, 'Need to pass rpath token to replace'
     sys.exit(1)
-    
-  old_rpath = args[0]
-  new_rpath = args[1]
+
+  path      = args[0]
+  old_rpath = args[1]
+  new_rpath = args[2]
 
   if len(old_rpath) < len(new_rpath):
     print >> sys.stderr, "New rpath must be shorter than old rpath"
@@ -32,7 +31,7 @@ def main(argv, stdout, environ):
   print 'Replacing: %s'%old_rpath
   print '     with: %s'%new_rpath
 
-  for root, dirs, files in os.walk(options.path):
+  for root, dirs, files in os.walk(path):
     for f in files:
 
       r = subprocess.Popen(['chrpath', os.path.join(root,f)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -43,7 +42,6 @@ def main(argv, stdout, environ):
         rp = o.split('RPATH=')[1].strip()
 
         if old_rpath in rp:
-          print 'Updating: %s'%(os.path.join(root,f))
           newrp = rp.replace(old_rpath, new_rpath)
           subprocess.check_call(['chrpath', os.path.join(root,f), '-r', newrp])
       
