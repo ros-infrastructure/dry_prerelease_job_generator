4e35b3f3c654b4760f47bb0e85c0fea1c94876ea
Ken Conley <kwc@kwc.org>
2010-09-10 23:27:53 +0000

    documentation

--- hudson/src/hudson.py
+++ hudson/src/hudson.py
@@ -1,12 +1,62 @@
 #!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2010, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+# $Author$
+
+"""
+Python API for Hudson
+
+Examples:
+
+    hudson.create_job('empty', EMPTY_CONFIG_XML)
+    hudson.disable_job('empty')
+    hudson.copy_job('empty', 'empty_copy')
+    hudson.enable_job('empty_copy')
+    hudson.reconfig_job('empty_copy', RECONFIG_XML)
+
+    hudson.delete_job('empty')
+    hudson.delete_job('empty_copy')
+
+    # build a parameterized job
+    hudson.build_job('api-test', {'param1': 'test value 1', 'param2': 'test value 2'})
+"""
+
 import sys
 import urllib2
 import urllib
 import base64
 import traceback
-
-SERVER = 'http://build.willowgarage.com/'
-#SERVER     = 'http://localhost:8080/'
 
 JOB_INFO     = 'job/%(name)s/api/python?depth=0'
 Q_INFO       = 'queue/api/python?depth=0'
@@ -66,6 +116,12 @@ def auth_headers(username, password):
 class Hudson(object):
     
     def __init__(self, url, username=None, password=None):
+        """
+        Create handle to Hudson instance.
+
+        @param url: URL of Hudson server
+        @type  url: str
+        """
         if url[-1] == '/':
             self.server = url
         else:
@@ -82,17 +138,24 @@ class Hudson(object):
             raise HudsonException("job[%s] does not exist"%name)
         
     def debug_job_info(self, job_name):
+        """
+        Print out job info in more readable format
+        """
         for k, v in self.get_job_info(job_name).iteritems():
             print k, v
 
     def hudson_open(self, req):
+        """
+        Utility routine for opening an HTTP request to a Hudson server. 
+        """
         try:
             if self.auth:
                 req.add_header('Authorization', self.auth)
             return urllib2.urlopen(req).read()
         except urllib2.HTTPError, e:
+            # Hudson's funky authentication means its nigh impossible to distinguish errors.
             if e.code in [401, 403, 500]:
-                raise HudsonException("authentication failed [%s]"%(e.code))
+                raise HudsonException("Error in request. Possibly authentication failed [%s]"%(e.code))
             # right now I'm getting 302 infinites on a successful delete
     
     def get_queue_info(self):
@@ -102,26 +165,57 @@ class Hudson(object):
         return eval(urllib2.urlopen(self.server + Q_INFO).read())['items']
 
     def copy_job(self, from_name, to_name):
+        """
+        Copy a Hudson job
+
+        @param from_name: Name of Hudson job to copy from
+        @type  from_name: str
+        @param to_name: Name of Hudson job to copy to
+        @type  to_name: str
+        """
         self.get_job_info(from_name)
         self.hudson_open(urllib2.Request(self.server + COPY_JOB%locals(), ''))
         if not self.job_exists(to_name):
             raise HudsonException("create[%s] failed"%(to_name))
 
     def delete_job(self, name):
+        """
+        Delete Hudson job permanently.
+        
+        @param name: Name of Hudson job
+        @type  name: str
+        """
         self.get_job_info(name)
         self.hudson_open(urllib2.Request(self.server + DELETE_JOB%locals(), ''))
         if self.job_exists(name):
             raise HudsonException("delete[%s] failed"%(name))
     
     def enable_job(self, name):
+        """
+        Enable Hudson job.
+
+        @param name: Name of Hudson job
+        @type  name: str
+        """
         self.get_job_info(name)
         self.hudson_open(urllib2.Request(self.server + ENABLE_JOB%locals(), ''))
 
     def disable_job(self, name):
+        """
+        Disable Hudson job. To re-enable, call enable_job().
+
+        @param name: Name of Hudson job
+        @type  name: str
+        """
         self.get_job_info(name)
         self.hudson_open(urllib2.Request(self.server + DISABLE_JOB%locals(), ''))
 
     def job_exists(self, name):
+        """
+        @param name: Name of Hudson job
+        @type  name: str
+        @return: True if Hudson job exists
+        """
         try:
             self.get_job_info(name)
             return True
@@ -130,6 +224,10 @@ class Hudson(object):
 
     def create_job(self, name, config_xml):
         """
+        Create a new Hudson job
+
+        @param name: Name of Hudson job
+        @type  name: str
         @param config_xml: config file text
         @type  config_xml: str
         """
@@ -142,6 +240,14 @@ class Hudson(object):
             raise HudsonException("create[%s] failed"%(name))
     
     def reconfig_job(self, name, config_xml):
+        """
+        Change configuration of existing Hudson job.
+
+        @param name: Name of Hudson job
+        @type  name: str
+        @param config_xml: New XML configuration
+        @type  config_xml: str
+        """
         self.get_job_info(name)
         headers = {'Content-Type': 'text/xml'}
         reconfig_url = self.server + RECONFIG_JOB%locals()
@@ -168,32 +274,3 @@ class Hudson(object):
                 req = urllib2.Request(self.server + BUILD_JOB%locals(), '')
             return self.hudson_open(req)
     
-def main():
-    if len(sys.argv) == 3:
-        username = sys.argv[1]
-        password = sys.argv[2]
-    elif len(sys.argv) == 1:
-        username = password = None
-    else:
-        if len(sys.argv) != 3:
-            print "Usage: hudson.py [username] [password]"
-            sys.exit(1)
-
-    hudson = Hudson(SERVER, username, password)
-    if 0:
-        if 0:
-            hudson.create_job('empty', EMPTY_CONFIG_XML)
-            hudson.disable_job('empty')
-            hudson.copy_job('empty', 'empty_copy')
-            hudson.enable_job('empty_copy')
-            if 0:
-                hudson.reconfig_job('empty_copy', RECONFIG_XML)
-        if 1:
-            hudson.delete_job('empty')
-            hudson.delete_job('empty_copy')
-    if 0:
-        hudson.build_job('api-test', {'param1': 'test value 1', 'param2': 'http://ros.org/wiki/distros/cturtle.rosdistro'})
-    
-if __name__ == '__main__':
-    main()
-
