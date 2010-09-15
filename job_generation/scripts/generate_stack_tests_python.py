12be3f3f79e8bd3e15a8da009ce3ff604c9cf96f
Wim Meeussen <wim@willowgarage.com>
2010-09-15 23:36:50 +0000

    push new python script to hudson

--- job_generation/scripts/generate_stack_tests_python.py
+++ job_generation/scripts/generate_stack_tests_python.py
@@ -50,12 +50,14 @@ echo "_________________________________BEGIN SCRIPT_____________________________
 cd /tmp/ros
 wget http://code.ros.org/svn/ros/installers/trunk/hudson/hudson_helper 
 wget http://code.ros.org/svn/ros/stacks/ros_release/trunk/job_generation/scripts/run_auto_stack_devel.py
+sudo apt-get install ros-ROSDISTRO-ros --yes
+source /opt/ros/ROSDISTRO/setup.sh
 export ROS_TEST_RESULTS_DIR=/tmp/ros/test_results
 export WORKSPACE=/tmp/ros
 export JOB_NAME=$JOB_NAME
 export BUILD_NUMBER=$BUILD_NUMBER
 export HUDSON_URL=$HUDSON_URL
-python /tmp/ros/run_auto_stack_devel --stack STACKNAME --rosdistro ROSDISTRO
+python run_auto_stack_devel.py --stack STACKNAME --rosdistro ROSDISTRO
 echo "_________________________________END SCRIPT_______________________________________"
 DELIM
 
@@ -124,6 +126,7 @@ cd $WORKSPACE &amp;&amp; $WORKSPACE/run_chroot.py --distro=UBUNTUDISTRO --arch=A
             <sendToRecipientList>true</sendToRecipientList> 
             <contentTypeHTML>false</contentTypeHTML> 
             <script>true</script> 
+
           </email> 
         </hudson.plugins.emailext.plugins.trigger.FixedTrigger> 
         <hudson.plugins.emailext.plugins.trigger.StillUnstableTrigger> 
@@ -226,43 +229,6 @@ def create_devel_configs(ubuntu_distros, arches, distro_name, stack_name, stack_
     return configs
 
 
-def create_prerelease_configs(ubuntu_distros, arches, distro_name, stack_name, stack_map, email):
-    # create list of stack dependencies
-    stack_file = urllib2.urlopen(stack_map[stack_name].dev_svn+'/stack.xml')
-    depends = roslib.stack_manifest.parse(stack_file.read()).depends
-    stack_file.close()
-
-    # create rosinstall file with all stacks that depend on this stack
-    rosinstall_depends_on = ""
-    for s in rospack.rosstack_depends_on(stack_name):
-        if s in stack_map:
-            rosinstall_config = get_rosinstall_config()
-            rosinstall_config = rosinstall_config.replace('STACKURI', stack_map[s].distro_svn)
-            rosinstall_config = rosinstall_config.replace('STACKNAME', s)
-            rosinstall_depends_on += rosinstall_config+'\n'
-
-    rosinstall_stack = get_rosinstall_config()
-    rosinstall_stack = rosinstall_stack.replace('STACKURI', stack_map[stack_name].dev_svn)
-    rosinstall_stack = rosinstall_stack.replace('STACKNAME', stack_name)
-
-    # create hudson config files for each ubuntu distro
-    configs = {}
-    for ubuntu in ubuntu_distros:
-        for arch in arches:
-            name = "_".join(['auto_stack_prerelease', distro_name, stack_name, ubuntu, arch])
-            hudson_config = HUDSON_PRERELEASE_CONFIG
-            hudson_config = hudson_config.replace('UBUNTUDISTRO', ubuntu)
-            hudson_config = hudson_config.replace('ARCH', arch)
-            hudson_config = hudson_config.replace('ROSDISTRO', distro_name)
-            hudson_config = hudson_config.replace('STACKNAME', stack_name)      
-            hudson_config = hudson_config.replace('STACKDEB', stack_to_deb(distro_name, stack_name))
-            hudson_config = hudson_config.replace('STACKDEPENDSDEB', ' '.join([stack_to_deb(distro_name, s) for s in depends]))
-            hudson_config = hudson_config.replace('STACKURI', stack_map[stack_name].dev_svn)
-            hudson_config = hudson_config.replace('ROSINSTALL_STACK', rosinstall_stack)
-            hudson_config = hudson_config.replace('ROSINSTALL_DEPENDS_ON', rosinstall_depends_on)
-            hudson_config = hudson_config.replace('EMAIL', email)
-            configs[name] = hudson_config
-    return configs
     
 
 def main():
@@ -315,32 +281,14 @@ def main():
 
     # generate hudson config files
     devel_configs = {}
-    prerelease_configs = {}
     if options.stacks:
         stack_list = options.stacks
     else:
         stack_list = distro_obj.stacks
     for stack_name in stack_list:
         devel_configs.update(create_devel_configs(ubuntudistro, archs, distro_obj.release_name, stack_name, distro_obj.stacks, options.email))
-        prerelease_configs.update(create_prerelease_configs(ubuntudistro, archs , distro_obj.release_name, stack_name, distro_obj.stacks, options.email))
     hudson_instance = hudson.Hudson(SERVER, username, password)
 
-
-    # send prerelease tests to Hudson
-    if options.prerelease:
-        for job_name in prerelease_configs:
-            exists = hudson_instance.job_exists(job_name)
-            if exists:
-                if options.delete or options.recreate:
-                    hudson_instance.delete_job(job_name)
-                    exists = False
-                    print "Deleting job %s"%job_name
-                elif not options.recreate:
-                    hudson_instance.reconfig_job(job_name, prerelease_configs[job_name])
-                    print "Reconfigure job %s"%job_name
-            if not options.delete and not exists:
-                print "Creating new job %s"%job_name
-                hudson_instance.create_job(job_name, prerelease_configs[job_name])
 
     # send devel tests to Hudson
     if options.devel:
