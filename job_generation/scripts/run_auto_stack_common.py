dcca829c4c9d7ff93ef7c296206cdc504d432b74
Wim Meeussen <wim@willowgarage.com>
2010-09-16 21:22:32 +0000

    fix

--- job_generation/scripts/run_auto_stack_common.py
+++ job_generation/scripts/run_auto_stack_common.py
@@ -20,6 +20,6 @@ def stack_to_rosinstall(stack, stack_map, svn='distro_svn'):
 def stacks_to_rosinstall(stack_list, stack_map, svn='distro_svn'):
     print stack_list
     if len(stack_list) == 0:
-        retrun ''
+        return ''
     return ' '.join([stack_to_rosinstall(s, stack_map, svn) for s in stack_list])
     
