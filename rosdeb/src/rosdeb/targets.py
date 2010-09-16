a66533b1bd3419d7cb0ad835a96d5024a5ddd1c4
Jeremy Leibs <jleibs@gmail.com>
2010-09-16 22:26:58 +0000

    Adding the testing target

--- rosdeb/src/rosdeb/targets.py
+++ rosdeb/src/rosdeb/targets.py
@@ -1,4 +1,5 @@
 os_platform = {
+    'testing': ['lucid', 'karmic', 'jaunty'],
     'unstable': ['lucid', 'karmic', 'jaunty'],
     'cturtle': ['lucid', 'karmic', 'jaunty'],
     'boxturtle': ['hardy', 'intrepid', 'jaunty', 'karmic'],    
