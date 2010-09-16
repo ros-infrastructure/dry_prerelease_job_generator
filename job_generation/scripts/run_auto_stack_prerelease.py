38c05cfad31a1d75a451a062e59e19d1c90ae497
Wim Meeussen <wim@willowgarage.com>
2010-09-16 21:01:01 +0000

    fix

--- job_generation/scripts/run_auto_stack_prerelease.py
+++ job_generation/scripts/run_auto_stack_prerelease.py
@@ -56,10 +56,8 @@ def main():
     for stack in options.stacklist:
         rosinstall += stack_to_rosinstall(stack, rosdistro_obj.stacks, 'dev_svn')
     for stack in options.stacklist:
-        res = subprocess.Popen(('rosstack depends-on %s'%stack).split(' '), stdout=subprocess.PIPE, env=env).communicate()
-        print res
-        if res:
-            rosinstall += stacks_to_rosinstall(res.split('\n'), rosdistro_obj.stacks)
+        res, err = subprocess.Popen(('rosstack depends-on %s'%stack).split(' '), stdout=subprocess.PIPE, env=env).communicate()
+        rosinstall += stacks_to_rosinstall(res.split('\n'), rosdistro_obj.stacks)
     print rosinstall
     rosinstall_file = 'depends_on_overlay.rosinstall'
     with open(rosinstall_file, 'w') as f:
