8c3f3edec9cf6179fcf511a3fef779a29242d521
Wim Meeussen <wim@willowgarage.com>
2010-09-17 17:13:48 +0000

    new script to create prerelease tests

---
+++ job_generation/scripts/generate_prerelease.py
@@ -0,0 +1,238 @@
+#!/usr/bin/python
+
+
+# template to create pre-release hudson configuration file
+HUDSON_PRERELEASE_CONFIG = """<?xml version='1.0' encoding='UTF-8'?>
+<project> 
+  <description>Pre-release build of STACKNAME for ROSDISTRO on UBUNTUDISTRO, ARCH</description> 
+ <logRotator> 
+    <daysToKeep>5</daysToKeep> 
+    <numToKeep>-1</numToKeep> 
+  </logRotator> 
+  <keepDependencies>false</keepDependencies> 
+  <properties> 
+    <hudson.plugins.trac.TracProjectProperty> 
+      <tracWebsite>http://code.ros.org/trac/ros/</tracWebsite> 
+    </hudson.plugins.trac.TracProjectProperty> 
+  </properties> 
+  <scm/>
+  <assignedNode>hudson-devel</assignedNode>
+  <canRoam>false</canRoam> 
+  <disabled>false</disabled> 
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding> 
+  <authToken>willow</authToken> 
+  <triggers class="vector"/> 
+  <concurrentBuild>false</concurrentBuild> 
+  <builders> 
+    <hudson.tasks.Shell> 
+      <command>cat &gt; $WORKSPACE/script.sh &lt;&lt;DELIM
+#!/usr/bin/env bash
+set -o errexit
+echo "_________________________________BEGIN SCRIPT______________________________________"
+export INSTALL_DIR=/tmp/install_dir
+export WORKSPACE=/tmp/ros
+export ROS_TEST_RESULTS_DIR=/tmp/ros/test_results
+export JOB_NAME=$JOB_NAME
+export BUILD_NUMBER=$BUILD_NUMBER
+export HUDSON_URL=$HUDSON_URL
+
+mkdir -p \$INSTALL_DIR
+cd \$INSTALL_DIR
+wget -m -nd http://code.ros.org/svn/ros/installers/trunk/hudson/hudson_helper 
+wget -m -nd http://code.ros.org/svn/ros/stacks/ros_release/trunk/job_generation/scripts/jobs_common.py 
+wget -m -nd http://code.ros.org/svn/ros/stacks/ros_release/trunk/job_generation/scripts/run_auto_stack_prerelease.py 
+
+sudo apt-get install ros-ROSDISTRO-ros --yes
+source /opt/ros/ROSDISTRO/setup.sh
+
+python run_auto_stack_prerelease.py STACKARGS --rosdistro ROSDISTRO
+echo "_________________________________END SCRIPT_______________________________________"
+DELIM
+
+set -o errexit
+
+wget https://code.ros.org/svn/ros/stacks/ros_release/trunk/hudson/scripts/run_chroot.py --no-check-certificate -O $WORKSPACE/run_chroot.py
+chmod +x $WORKSPACE/run_chroot.py
+cd $WORKSPACE &amp;&amp; $WORKSPACE/run_chroot.py --distro=UBUNTUDISTRO --arch=ARCH  --ramdisk --script=$WORKSPACE/script.sh
+     </command> 
+    </hudson.tasks.Shell> 
+  </builders> 
+  <publishers> 
+    <hudson.tasks.junit.JUnitResultArchiver> 
+      <testResults>test_results/_hudson/*.xml</testResults> 
+    </hudson.tasks.junit.JUnitResultArchiver> 
+    <hudson.plugins.emailext.ExtendedEmailPublisher> 
+      <recipientList>EMAIL</recipientList> 
+      <configuredTriggers> 
+        <hudson.plugins.emailext.plugins.trigger.UnstableTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.UnstableTrigger> 
+        <hudson.plugins.emailext.plugins.trigger.FailureTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.FailureTrigger> 
+        <hudson.plugins.emailext.plugins.trigger.StillFailingTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.StillFailingTrigger> 
+        <hudson.plugins.emailext.plugins.trigger.FixedTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.FixedTrigger> 
+        <hudson.plugins.emailext.plugins.trigger.StillUnstableTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.StillUnstableTrigger> 
+      </configuredTriggers> 
+      <defaultSubject>$DEFAULT_SUBJECT</defaultSubject> 
+      <defaultContent>$DEFAULT_CONTENT&#xd;
+&#xd;
+&lt;% &#xd;
+def ws = build.getParent().getWorkspace()&#xd;
+def computer = build.getExecutor().getOwner()&#xd;
+def build_failures = hudson.util.RemotingDiagnostics.executeGroovy(&quot;new File(\&quot;${ws}/build_output/buildfailures.txt\&quot;).text&quot;,computer.getChannel())&#xd;
+println &quot;${build_failures}&quot;&#xd;
+def test_failures = hudson.util.RemotingDiagnostics.executeGroovy(&quot;new File(\&quot;${ws}/test_output/testfailures.txt\&quot;).text&quot;,computer.getChannel())&#xd;
+println &quot;${test_failures}&quot;&#xd;
+def build_failures_context = hudson.util.RemotingDiagnostics.executeGroovy(&quot;new File(\&quot;${ws}/build_output/buildfailures-with-context.txt\&quot;).text&quot;,computer.getChannel())&#xd;
+println &quot;${build_failures_context}&quot;&#xd;
+%&gt;</defaultContent> 
+      <defaultContentTypeHTML>false</defaultContentTypeHTML> 
+      <defaultContentIsScript>true</defaultContentIsScript> 
+    </hudson.plugins.emailext.ExtendedEmailPublisher> 
+  </publishers> 
+  <buildWrappers/> 
+</project>
+"""
+
+
+import roslib; roslib.load_manifest("hudson")
+from roslib import distro
+from jobs_common import *
+import hudson
+import sys
+import re
+import urllib2
+import optparse 
+
+
+
+
+def create_prerelease_configs(rosdistro, stack_list, stack_map, email):
+    # create hudson config files for each ubuntu distro
+    configs = {}
+    for ubuntudistro in UBUNTU_DISTRO_MAP[rosdistro]:
+        for arch in ARCHES:
+            name = "_".join(['prerelease', rosdistro, '_'.join(stack_list), ubuntudistro, arch])
+            hudson_config = HUDSON_PRERELEASE_CONFIG
+            hudson_config = hudson_config.replace('UBUNTUDISTRO', ubuntudistro)
+            hudson_config = hudson_config.replace('ARCH', arch)
+            hudson_config = hudson_config.replace('ROSDISTRO', rosdistro)
+            hudson_config = hudson_config.replace('STACKNAME', '---'.join(stack_list))
+            hudson_config = hudson_config.replace('STACKARGS', ' '.join(['--stack %s'%s for s in stack_list]))
+            hudson_config = hudson_config.replace('EMAIL', email)
+            configs[name] = hudson_config
+    return configs
+    
+    
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option('--delete', dest = 'delete', default=False, action='store_true',
+                      help='Delete jobs from Hudson')    
+    parser.add_option('--stack', dest = 'stacks', action='append',
+                      help="Specify the stacks to operate on (defaults to all stacks)")
+    parser.add_option('--rosdistro', dest = 'rosdistro', action='store',
+                      help="Specify the ros distro to operate on (defaults to cturtle)")
+    parser.add_option('--email', dest='email', action='store',
+                      help='Send email to this address')
+    (options, args) = parser.parse_args()
+    if not options.email:
+        print 'Please provide your email address: --email you@willowgarage.com'
+        return
+    if not options.stacks:
+        print 'Please provide at least one stack to test: --stack pr2_doors'
+        return
+    if not options.rosdistro:
+        print 'Please provide the ros distro you want to test: --rosdistro cturtle'
+        return
+
+    # Parse distro file
+    distro_obj = distro.Distro(ROSDISTRO_MAP[options.rosdistro])
+    print 'Operating on ROS distro %s'%distro_obj.release_name
+
+    # parse username and password
+    if len(args) != 2:
+        parser.error('Needs username and password as args')
+    username = args[0]
+    password = args[1]
+
+    # generate hudson config files
+    prerelease_configs = create_prerelease_configs(distro_obj.release_name, options.stacks, distro_obj.stacks, options.email)
+    hudson_instance = hudson.Hudson(SERVER, username, password)
+
+    # send prerelease tests to Hudson
+    for job_name in prerelease_configs:
+        exists = hudson_instance.job_exists(job_name)
+
+        # delete job
+        if options.delete and exists:
+            hudson_instance.delete_job(job_name)
+            print "Deleting job %s"%job_name
+
+        # reconfigure job
+        elif exists:
+            hudson_instance.reconfig_job(job_name, prerelease_configs[job_name])
+            hudson_instance.build_job(job_name)
+            print "Reconfigure job %s"%job_name
+
+        # create job
+        elif not exists:
+            hudson_instance.create_job(job_name, prerelease_configs[job_name])
+            hudson_instance.build_job(job_name)
+            print "Creating new job %s"%job_name
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
