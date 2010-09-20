b7d85fbd743425d7621dff7c26ac5885aec5444f
Wim Meeussen <wim@willowgarage.com>
2010-09-20 17:24:54 +0000

    test

--- job_generation/scripts/run_auto_stack_devel.py
+++ job_generation/scripts/run_auto_stack_devel.py
@@ -62,6 +62,7 @@ def main():
     # Start Hudson Helper
     print 'Running Hudson Helper'
     test_dir = env['WORKSPACE']+'/'+options.stack
+    subprocess.Popen(('chmod +x hudson_helper').split(' '), env=env).communicate()    
     return subprocess.Popen(('./hudson_helper --dir-test %s build'%test_dir).split(' '), env=env).communicate()
 
 
