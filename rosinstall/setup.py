4a1402df370821b8296551d078da82776173ca67
Tully Foote <tfoote@willowgarage.com>
2010-09-28 01:00:29 +0000

    reving revison number

--- rosinstall/setup.py
+++ rosinstall/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='rosinstall',
-      version='0.5.7',
+      version='0.5.8',
       packages=['rosinstall', 'rosinstall.vcs'],
       package_dir = {'':'src'},
       scripts = ["scripts/rosinstall"],
