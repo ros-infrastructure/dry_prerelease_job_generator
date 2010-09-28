ffc7e2fa820308a6cb95106b4fc213293e156887
Wim Meeussen <wim@willowgarage.com>
2010-09-28 20:29:22 +0000

    released 0.1.3

--- job_generation/export/ros-prerelease/setup.py
+++ job_generation/export/ros-prerelease/setup.py
@@ -2,12 +2,12 @@
 from distutils.core import setup
 
 setup(name='ros-prerelease',
-      version='0.1.1',
+      version='0.1.3',
       scripts = ["ros-prerelease.py", "ros_prerelease_jobs_common.py", "ros_prerelease_hudson.py"],
       author = "Wim Meeussen", 
       author_email = "wim@willowgarage.com",
       url = "http://www.ros.org/wiki/ros-prerelease",
-      download_url = "http://pypi.python.org/packages/source/r/ros-prerelease/ros-prerelease-0.1.1.tar.gz", 
+      download_url = "http://pypi.python.org/packages/source/r/ros-prerelease/ros-prerelease-0.1.3.tar.gz", 
       keywords = ["ROS"],
       classifiers = [
         "Programming Language :: Python", 
