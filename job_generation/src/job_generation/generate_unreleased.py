5423f56c8b21c56bc1924df731b4eb8d8d83f635
Wim Meeussen <wim@willowgarage.com>
2010-10-01 20:21:36 +0000

    test unreleased tests

---
+++ job_generation/src/job_generation/generate_unreleased.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python
+
+
+# template to create unreleased hudson configuration file
+HUDSON_UNRELEASED_CONFIG = """<?xml version='1.0' encoding='UTF-8'?>
+<project> 
+  <description>Unreleased build of NAME for ROSDISTRO on UBUNTUDISTRO, ARCH</description> 
+ <logRotator> 
+    <daysToKeep>5</daysToKeep> 
+    <numToKeep>-1</numToKeep> 
+  </logRotator> 
+  <keepDependencies>false</keepDependencies> 
+  <properties> 
+    <hudson.plugins.trac.TracProjectProperty> 
+      <tracWebsite>http://code.ros.org/trac/ros/</tracWebsite> 
+    </hudson.plugins.trac.TracProjectProperty> 
+  </properties> 
+  <scm/>
+  <assignedNode>hudson-devel</assignedNode>
+  <canRoam>false</canRoam> 
+  <disabled>false</disabled> 
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding> 
+  <triggers class="vector"/> 
+  <concurrentBuild>false</concurrentBuild> 
+  <builders> 
+    <hudson.tasks.Shell> 
+      <command>cat &gt; $WORKSPACE/script.sh &lt;&lt;DELIM
+#!/usr/bin/env bash
+set -o errexit
+echo "_________________________________BEGIN SCRIPT______________________________________"
+sudo apt-get install ros-ROSDISTRO-ros --yes
+source /opt/ros/ROSDISTRO/setup.sh
+
+export INSTALL_DIR=/tmp/install_dir
+export WORKSPACE=/tmp/ros
+export ROS_TEST_RESULTS_DIR=/tmp/ros/test_results
+export JOB_NAME=$JOB_NAME
+export BUILD_NUMBER=$BUILD_NUMBER
+export HUDSON_URL=$HUDSON_URL
+export ROS_PACKAGE_PATH=\$INSTALL_DIR/ros_release:/opt/ros/ROSDISTRO/stacks
+
+mkdir -p \$INSTALL_DIR
+cd \$INSTALL_DIR
+
+wget -m -nd http://code.ros.org/svn/ros/installers/trunk/hudson/hudson_helper 
+chmod +x hudson_helper
+svn co https://code.ros.org/svn/ros/stacks/ros_release/trunk ros_release
+./ros_release/job_generation/src/job_generation/run_auto_stack_unreleased.py --rosinstall ROSINSTALL --rosdistro ROSDISTRO
+
+echo "_________________________________END SCRIPT_______________________________________"
+DELIM
+
+set -o errexit
+
+rm -rf $WORKSPACE/test_results
+rm -rf $WORKSPACE/test_output
+
+wget https://code.ros.org/svn/ros/stacks/ros_release/trunk/hudson/scripts/run_chroot.py --no-check-certificate -O $WORKSPACE/run_chroot.py
+chmod +x $WORKSPACE/run_chroot.py
+cd $WORKSPACE &amp;&amp; $WORKSPACE/run_chroot.py --distro=UBUNTUDISTRO --arch=ARCH  --ramdisk --script=$WORKSPACE/script.sh
+     </command> 
+    </hudson.tasks.Shell> 
+  </builders> 
+  <publishers> 
+    <hudson.tasks.BuildTrigger> 
+      <childProjects>JOB_CHILDREN</childProjects> 
+      <threshold> 
+        <name>SUCCESS</name> 
+        <ordinal>0</ordinal> 
+        <color>BLUE</color> 
+      </threshold> 
+    </hudson.tasks.BuildTrigger> 
+    <hudson.tasks.junit.JUnitResultArchiver> 
+      <testResults>test_results/_hudson/*.xml</testResults> 
+    </hudson.tasks.junit.JUnitResultArchiver> 
+    <hudson.plugins.emailext.ExtendedEmailPublisher> 
+      <recipientList>EMAIL</recipientList> 
+      <configuredTriggers> 
+        <hudson.plugins.emailext.plugins.trigger.UnstableTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.UnstableTrigger> 
+        <hudson.plugins.emailext.plugins.trigger.FailureTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.FailureTrigger> 
+        <hudson.plugins.emailext.plugins.trigger.StillFailingTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.StillFailingTrigger> 
+        <hudson.plugins.emailext.plugins.trigger.FixedTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.FixedTrigger> 
+        <hudson.plugins.emailext.plugins.trigger.StillUnstableTrigger> 
+          <email> 
+            <recipientList></recipientList> 
+            <subject>$PROJECT_DEFAULT_SUBJECT</subject> 
+            <body>$PROJECT_DEFAULT_CONTENT</body> 
+            <sendToDevelopers>true</sendToDevelopers> 
+            <sendToRecipientList>true</sendToRecipientList> 
+            <contentTypeHTML>false</contentTypeHTML> 
+            <script>true</script> 
+          </email> 
+        </hudson.plugins.emailext.plugins.trigger.StillUnstableTrigger> 
+      </configuredTriggers> 
+      <defaultSubject>$DEFAULT_SUBJECT</defaultSubject> 
+      <defaultContent>$DEFAULT_CONTENT&#xd;
+&#xd;
+&lt;% &#xd;
+def ws = build.getParent().getWorkspace()&#xd;
+def computer = build.getExecutor().getOwner()&#xd;
+def build_failures = hudson.util.RemotingDiagnostics.executeGroovy(&quot;new File(\&quot;${ws}/build_output/buildfailures.txt\&quot;).text&quot;,computer.getChannel())&#xd;
+println &quot;${build_failures}&quot;&#xd;
+def test_failures = hudson.util.RemotingDiagnostics.executeGroovy(&quot;new File(\&quot;${ws}/test_output/testfailures.txt\&quot;).text&quot;,computer.getChannel())&#xd;
+println &quot;${test_failures}&quot;&#xd;
+def build_failures_context = hudson.util.RemotingDiagnostics.executeGroovy(&quot;new File(\&quot;${ws}/build_output/buildfailures-with-context.txt\&quot;).text&quot;,computer.getChannel())&#xd;
+println &quot;${build_failures_context}&quot;&#xd;
+%&gt;</defaultContent> 
+      <defaultContentTypeHTML>false</defaultContentTypeHTML> 
+      <defaultContentIsScript>true</defaultContentIsScript> 
+    </hudson.plugins.emailext.ExtendedEmailPublisher> 
+  </publishers> 
+  <buildWrappers/> 
+</project>
+"""
+
+from ros_prerelease_jobs_common import *
+import ros_unreleased_hudson
+import urllib
+import optparse 
+
+
+
+
+def create_unreleased_configs(rosdistro, rosinstall, email):
+    # create hudson config files for each ubuntu distro
+    configs = {}
+    for ubuntudistro in UBUNTU_DISTRO_MAP[rosdistro]:
+        for arch in ARCHES:
+            name = "_".join(['unreleased', rosdistro, '_'.join(stack_list), ubuntudistro, arch])
+            hudson_config = HUDSON_UNRELEASED_CONFIG
+            hudson_config = hudson_config.replace('UBUNTUDISTRO', ubuntudistro)
+            hudson_config = hudson_config.replace('ARCH', arch)
+            hudson_config = hudson_config.replace('ROSDISTRO', rosdistro)
+            hudson_config = hudson_config.replace('ROSINSTALL', rosinstall)
+            hudson_config = hudson_config.replace('EMAIL', 'wim+hudson_auto_stack@willowgarage.com')
+            configs[name] = hudson_config
+    return configs
+    
+    
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option('--delete', dest = 'delete', default=False, action='store_true',
+                      help='Delete jobs from Hudson')    
+    parser.add_option('--rosinstall', dest = 'rosinstall', action='store',
+                      help="Specify the rosinstall file that refers to the unreleased code.")
+    parser.add_option('--rosdistro', dest = 'rosdistro', action='store',
+                      help="Specify the ros distro to operate on (defaults to cturtle)")
+    (options, args) = parser.parse_args()
+    if not options.rosdistro:
+        print 'Please provide the ros distro you want to test: --rosdistro cturtle'
+        return
+    if not options.rosdistro in UBUNTU_DISTRO_MAP.keys():
+        print 'You profided an invalid "--rosdistro %s" argument. Options are %s'%(options.rosdistro, UBUNTU_DISTRO_MAP.keys())
+        return
+    if not options.rosinstall:
+        print 'Please provide the rosinstall of unreleased code to test: --rosinstall foo.rosinstall'
+        return
+        
+
+    # hudson instance
+    info = urllib.urlopen(CONFIG_PATH).read().split(',')
+    hudson_instance = ros_unreleased_hudson.Hudson(SERVER, info[0], info[1])
+
+    # send unreleased tests to Hudson
+    print 'Creating unreleased Hudson jobs:'
+    unreleased_configs = create_unreleased_configs(options.rosdistro, options.stacks, options.email)
+    for job_name in unreleased_configs:
+        exists = hudson_instance.job_exists(job_name)
+
+        # delete job
+        if options.delete and exists:
+            hudson_instance.delete_job(job_name)
+            print "Deleting job %s"%job_name
+
+        # reconfigure job
+        elif exists:
+            hudson_instance.reconfig_job(job_name, unreleased_configs[job_name])
+            hudson_instance.build_job(job_name)
+            print "  - %s"%job_name
+
+        # create job
+        elif not exists:
+            hudson_instance.create_job(job_name, unreleased_configs[job_name])
+            hudson_instance.build_job(job_name)
+            print "  - %s"%job_name
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ job_generation/src/job_generation/run_auto_stack_unreleased.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+STACK_DIR = 'stack_overlay'
+ROSINSTALL_FILE = 'unreleased.rosinstall'
+
+
+import roslib; roslib.load_manifest("job_generation")
+from roslib import stack_manifest
+from roslib2 import distro
+from jobs_common import *
+import sys
+import os
+import optparse 
+import subprocess
+
+
+def main():
+    # parse command line options
+    parser = optparse.OptionParser()
+    
+    parser.add_option('--rosinstall', dest = 'rosinstall', action='store',
+                      help='Stack name')
+    parser.add_option('--rosdistro', dest = 'rosdistro', default=False, action='store',
+                      help='Ros distro name')
+    (options, args) = parser.parse_args()
+    if not options.rosinstall or not options.rosdistro:
+        print 'You did not specify all options to run this script.'
+        return
+
+
+    # set environment
+    env = {}
+    env['WORKSPACE'] = os.environ['WORKSPACE']
+    env['HOME'] = os.environ['WORKSPACE']
+    env['JOB_NAME'] = os.environ['JOB_NAME']
+    env['BUILD_NUMBER'] = os.environ['BUILD_NUMBER']
+    env['PWD'] = os.environ['WORKSPACE']
+    env['PATH'] = '/opt/ros/%s/ros/bin:%s'%(options.rosdistro, os.environ['PATH'])
+    env['ROS_PACKAGE_PATH'] = '%s:/opt/ros/%s/stacks'%(os.environ['INSTALL_DIR']+'/'+STACK_DIR,
+                                                       options.rosdistro)
+    env['ROS_ROOT'] = '/opt/ros/%s/ros'%options.rosdistro
+    env['PYTHONPATH'] = env['ROS_ROOT']+'/core/roslib/src'
+
+
+    # Parse distro file
+    rosdistro_obj = distro.Distro(ROSDISTRO_MAP[options.rosdistro])
+    print 'Operating on ROS distro %s'%rosdistro_obj.release_name
+
+    # Install Debian packages of ALL stacks in distro
+    print 'Installing all stacks of ros distro %s: %s'%(options.rosdistro, str(rosdistro_obj.stacks.keys()))
+    for stack in rosdistro_obj.stacks:
+        subprocess.Popen(('sudo apt-get install %s --yes'%(stack_to_deb(stack, options.rosdistro))).split(' ')).communicate()
+    
+
+    # Install unreleased code to test
+    with open(ROSINSTALL_FILE, 'w') as f:
+        f.write(urllib2.urlopen(options.rosinstall).read())
+    subprocess.Popen(('rosinstall %s /opt/ros/%s %s'%(STACK_DIR, options.rosdistro, ROSINSTALL_FILE)).split(' ')).communicate()
+
+
+    # Run hudson helper 
+    print 'Running Hudson Helper'
+    helper = subprocess.Popen(('./hudson_helper --dir-test %s build'%STACK_DIR).split(' '), env=env)
+    helper.communicate()
+    return helper.returncode
+
+
+if __name__ == '__main__':
+    sys.exit( main() )
+
+
+
+
+
