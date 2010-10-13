99173710b5e355bf705b6e237390875d1a145f79
Tully Foote <tfoote@willowgarage.com>
2010-10-13 20:05:06 +0000

    removing ramdisk from prerelease for now

--- job_generation/src/job_generation/generate_prerelease.py
+++ job_generation/src/job_generation/generate_prerelease.py
@@ -57,7 +57,7 @@ rm -rf $WORKSPACE/test_output
 
 wget  --no-check-certificate https://code.ros.org/svn/ros/stacks/ros_release/trunk/hudson/scripts/run_chroot.py -O $WORKSPACE/run_chroot.py
 chmod +x $WORKSPACE/run_chroot.py
-cd $WORKSPACE &amp;&amp; $WORKSPACE/run_chroot.py --distro=UBUNTUDISTRO --arch=ARCH  --ramdisk --script=$WORKSPACE/script.sh
+cd $WORKSPACE &amp;&amp; $WORKSPACE/run_chroot.py --distro=UBUNTUDISTRO --arch=ARCH --script=$WORKSPACE/script.sh
      </command> 
     </hudson.tasks.Shell> 
   </builders> 
