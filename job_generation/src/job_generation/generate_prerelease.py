c81dcf84f4fda142ea92648407634252ef4c5c77
Wim Meeussen <wim@willowgarage.com>
2010-10-27 00:01:13 +0000

    add check to see if stack exists before submitting job

--- job_generation/src/job_generation/generate_prerelease.py
+++ job_generation/src/job_generation/generate_prerelease.py
@@ -211,6 +211,12 @@ def main():
     if not options.stacks:
         print 'Please provide at least one stack to test: --stack pr2_doors'
         return
+    distro_obj = rosdistro.Distro(ROSDISTRO_MAP[options.rosdistro])
+    for s in options.stacks:
+        if not s in distro_obj.stacks:
+            print 'Stack %s does not exist in the %s disro file. Before you can run the prerelease scripts you need to add this stack to the rosdistro file'%(s, options.rosdistro)
+            return
+
         
     # create hudson instance
     if len(args) == 2:
