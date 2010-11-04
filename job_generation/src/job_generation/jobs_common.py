995dadf8ccbce0b7952376bd49723f4706bdce6f
Tully Foote <tfoote@willowgarage.com>
2010-11-04 18:13:13 +0000

    adding maverick to unstable tests

--- job_generation/src/job_generation/jobs_common.py
+++ job_generation/src/job_generation/jobs_common.py
@@ -12,7 +12,7 @@ ROSDISTRO_MAP = {'unstable': 'https://code.ros.org/svn/release/trunk/distros/uns
 ARCHES = ['amd64', 'i386']
 
 # ubuntu distro mapping to ros distro
-UBUNTU_DISTRO_MAP = {'unstable': ['lucid'],
+UBUNTU_DISTRO_MAP = {'unstable': ['lucid', 'maverick'],
                      'cturtle':  ['lucid', 'karmic', 'jaunty'],
                      'boxturtle':['hardy','karmic', 'jaunty']}
 
