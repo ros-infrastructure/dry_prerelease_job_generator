cffa45d2fbf270069be8700ee22fcb9807572894
Tully Foote <tfoote@willowgarage.com>
2010-11-12 23:00:05 +0000

    printing more and removing local url

--- rosdistro/test/test_gen.py
+++ rosdistro/test/test_gen.py
@@ -3,14 +3,14 @@ import rosdistro
 import yaml
 
 
-release_name = 'cturtle'
-uri = "/home/tfoote/ros-release/release/distros/%s.rosdistro"%release_name
+release_name = 'unstable'
 uri = "https://code.ros.org/svn/release/trunk/distros/%s.rosdistro"%release_name
 
 distro = rosdistro.Distro(uri)
 
 
 print 'ros', distro.stacks['ros'].repo
+print 'nxt', distro.stacks['nxt'].repo
 print 'pr2_doors', distro.stacks['pr2_doors'].repo
 
 print 'geometry', distro.stacks['geometry'].repo
@@ -21,6 +21,10 @@ for s in distro.stack_names:
 
 print "geometry"
 print yaml.dump(rosdistro.stack_to_rosinstall(distro.stacks['geometry'], "distro"))
+print "nxt distro"
+print yaml.dump(rosdistro.stack_to_rosinstall(distro.stacks['nxt'], "distro"))
+print "nxt release"
+print yaml.dump(rosdistro.stack_to_rosinstall(distro.stacks['nxt'], "release"))
 print "pr2all"
 print yaml.dump( rosdistro.variant_to_rosinstall("pr2all", distro, "distro"))
 print "distro"
