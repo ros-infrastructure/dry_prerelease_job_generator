32b4ac1d06d6b499c1cc39607269778f3b00b0cb
Tully Foote <tfoote@willowgarage.com>
2010-11-12 21:34:13 +0000

    a non unit test test script for now

---
+++ rosdistro/test/test_gen.py
@@ -0,0 +1,29 @@
+import roslib; roslib.load_manifest("rosdistro")
+import rosdistro
+import yaml
+
+
+release_name = 'cturtle'
+uri = "/home/tfoote/ros-release/release/distros/%s.rosdistro"%release_name
+uri = "https://code.ros.org/svn/release/trunk/distros/%s.rosdistro"%release_name
+
+distro = rosdistro.Distro(uri)
+
+
+print 'ros', distro.stacks['ros'].repo
+print 'pr2_doors', distro.stacks['pr2_doors'].repo
+
+print 'geometry', distro.stacks['geometry'].repo
+
+
+for s in distro.stack_names:
+    print s, distro.stacks[s].repo
+
+print "geometry"
+print yaml.dump(rosdistro.stack_to_rosinstall(distro.stacks['geometry'], "distro"))
+print "pr2all"
+print yaml.dump( rosdistro.variant_to_rosinstall("pr2all", distro, "distro"))
+print "distro"
+print yaml.dump( rosdistro.distro_to_rosinstall(distro, "distro"))
+
+
