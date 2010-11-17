5ad4acfa4d0cc58f3e2be61fe7e65997d2084c0f
Ken Conley <kwc@kwc.org>
2010-11-17 19:00:09 +0000

    enabling maverick on cturtle

--- rosdeb/src/rosdeb/targets.py
+++ rosdeb/src/rosdeb/targets.py
@@ -1,6 +1,6 @@
 os_platform = {
     'testing': ['lucid', 'maverick'],
     'unstable': ['lucid', 'maverick'],
-    'cturtle': ['lucid', 'karmic', 'jaunty'],
+    'cturtle': ['maverick', 'lucid', 'karmic', 'jaunty'],
     'boxturtle': ['hardy', 'intrepid', 'jaunty', 'karmic'],    
 }
