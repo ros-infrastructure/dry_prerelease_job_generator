f3936db05430445de29720c4e0648dc76c23f1a1
Tully Foote <tfoote@willowgarage.com>
2010-11-24 18:51:21 +0000

    wait for return code

--- hudson/scripts/devel_run_chroot.py
+++ hudson/scripts/devel_run_chroot.py
@@ -29,6 +29,10 @@ def local_check_call(cmd, display_output=False):
         if not l:
             break
         print l, ##extra comma because lines already have \n.  I"m assuming this is lower overhead than l.strip()
+
+    if p.returncode == None:
+        print "stdout finished but process not exited!!!"
+        p.communicate()
     if p.returncode != 0:
         raise subprocess.CalledProcessError(p.returncode, cmd)
 
