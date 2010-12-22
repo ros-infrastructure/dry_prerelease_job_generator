a49b1321a4aa0d0a4b6173e3b762b0a2c1ca6f66
Wim Meeussen <wim@willowgarage.com>
2010-12-22 21:38:11 +0000

    script to create rosinstalls of variant

---
+++ job_generation/scripts/generate_rosinstall.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+PKG = 'job_generation'
+import roslib; roslib.load_manifest(PKG)
+
+import sys
+import subprocess
+import rosdistro 
+import optparse
+from job_generation.jobs_common import *
+
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option('--overlay', dest = 'overlay', default=False, action='store_true',
+                      help='Create overlay file')    
+    parser.add_option('--variant', dest = 'variant', action='store',
+                      help="Specify the variant to create a rosinstall for")
+    parser.add_option('--rosdistro', dest = 'rosdistro', action='store', 
+                      help="Specify the ros distro to operate on")
+    (options, args) = parser.parse_args()
+    if not options.rosdistro in UBUNTU_DISTRO_MAP.keys():
+        print 'Rosdistro %s does not exist. Options are %s'%(options.rosdistro, UBUNTU_DISTRO_MAP.keys())
+        return
+
+    # Parse distro file
+    distro_obj = rosdistro.Distro(ROSDISTRO_MAP[options.rosdistro])
+    
+
+    # check if variant exists
+    if not options.variant in distro_obj.variants:
+        print 'Variant %s does not exist in rosdistro %s'%(options.variant, options.rosdistro)
+        return
+
+    # generate rosinstall file for variant
+    if options.overlay:
+        rosinstall = rosdistro.variant_to_rosinstall(options.variant, distro_obj, 'release')
+    else:
+        rosinstall = rosdistro.extended_variant_to_rosinstall(options.variant, distro_obj, 'release')        
+
+    print rosinstall
+
+if __name__ == '__main__':
+    main()
