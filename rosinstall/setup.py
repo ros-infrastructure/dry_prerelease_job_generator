d56298d37dc9fb76cee78aca506e48f8577545ae
Tully Foote <tfoote@willowgarage.com>
2011-01-11 02:13:41 +0000

    reving revision number

--- rosinstall/setup.py
+++ rosinstall/setup.py
@@ -2,14 +2,14 @@
 from distutils.core import setup
 
 setup(name='rosinstall',
-      version='0.5.9',
+      version='0.5.10',
       packages=['rosinstall', 'rosinstall.vcs'],
       package_dir = {'':'src'},
       scripts = ["scripts/rosinstall"],
       author = "Tully Foote", 
       author_email = "tfoote@willowgarage.com",
       url = "http://www.ros.org/wiki/rosinstall",
-      download_url = "http://pypi.python.org/packages/source/r/roslib2/roslib2-0.01.tar.gz", 
+      download_url = "http://pr.willowgarage.com/downloads/rosinstall/", 
       keywords = ["ROS"],
       classifiers = [
         "Programming Language :: Python", 
