692805e1cc0145a32517806caf3070e384b826c4
Tully Foote <tfoote@willowgarage.com>
2011-01-12 21:32:26 +0000

    using jeremy's bash and simple version for zsh

--- rosinstall/setup.py
+++ rosinstall/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='rosinstall',
-      version='0.5.11',
+      version='0.5.12',
       packages=['rosinstall', 'rosinstall.vcs'],
       package_dir = {'':'src'},
       scripts = ["scripts/rosinstall"],
