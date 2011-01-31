33140c400036ca1fecaf83001e1c0748bc5c6233
Wim Meeussen <wim@willowgarage.com>
2011-01-31 21:43:07 +0000

    woking locally

--- job_generation/cgi-bin/gen_rosinstall.py
+++ job_generation/cgi-bin/gen_rosinstall.py
@@ -6,6 +6,7 @@ import urllib
 import os
 import subprocess
 import tempfile
+import re
 
 import cgitb
 cgitb.enable()
@@ -35,8 +36,10 @@ def main():
          print 'invalid variant parameter'
          return
 
-    command = 'ssh -i /keys/id_dsa willow@pub8 export ROS_PACKAGE_PATH="/home/willow/ros_release:/opt/ros/cturtle/stacks" && export ROS_ROOT="/opt/ros/cturtle/ros" && export PATH="/opt/ros/cturtle/ros/bin:$PATH" && export PYTHONPATH="/opt/ros/cturtle/ros/core/roslib/src" && rosrun job_generation generate_rosinstall.py --rosdistro %s --variant %s --overlay %s --database /home/willow/rosinstall.db'%(form['rosdistro'].value, form['variant'].value, form['overlay'].value)
-    res, err = subprocess.Popen(command.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    command = 'export ROS_HOME=/tmp && export ROS_PACKAGE_PATH="/home/willow/ros_release:/opt/ros/cturtle/stacks" && export ROS_ROOT="/opt/ros/cturtle/ros" && export PATH="/opt/ros/cturtle/ros/bin:$PATH" && export PYTHONPATH="/opt/ros/cturtle/ros/core/roslib/src" && rosrun job_generation generate_rosinstall.py --rosdistro %s --variant %s --overlay %s --database /home/willow/rosinstall.db'%(form['rosdistro'].value, form['variant'].value, form['overlay'].value)
+
+
+    res, err = subprocess.Popen(['bash', '-c', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     print '%s %s'%(str(res), str(err))    
 
 
