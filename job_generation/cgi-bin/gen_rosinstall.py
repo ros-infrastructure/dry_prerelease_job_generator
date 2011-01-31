be0a4a3239be65a6001f662a6fd9183ffcde9c88
Ken Conley <kwc@kwc.org>
2011-01-31 08:31:17 +0000

    re-based validation on variant

--- job_generation/cgi-bin/gen_rosinstall.py
+++ job_generation/cgi-bin/gen_rosinstall.py
@@ -29,6 +29,11 @@ def main():
          # needs to send httperror instead
          print 'invalid overlay parameter'
          return
+    p = re.compile('\A[A-Za-z]+[\w\-]*\Z')
+    if not bool(p.match(form['variant'].value)):
+         # needs to send httperror instead
+         print 'invalid variant parameter'
+         return
 
     command = 'ssh -i /keys/id_dsa willow@pub8 export ROS_PACKAGE_PATH="/home/willow/ros_release:/opt/ros/cturtle/stacks" && export ROS_ROOT="/opt/ros/cturtle/ros" && export PATH="/opt/ros/cturtle/ros/bin:$PATH" && export PYTHONPATH="/opt/ros/cturtle/ros/core/roslib/src" && rosrun job_generation generate_rosinstall.py --rosdistro %s --variant %s --overlay %s --database /home/willow/rosinstall.db'%(form['rosdistro'].value, form['variant'].value, form['overlay'].value)
     res, err = subprocess.Popen(command.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
