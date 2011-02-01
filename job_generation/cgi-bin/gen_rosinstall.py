55e2cb09fac71cfda313fb0794d25fb457640716
Wim Meeussen <wim@willowgarage.com>
2011-02-01 17:09:47 +0000

    sys.exit(-1) on failure

--- job_generation/cgi-bin/gen_rosinstall.py
+++ job_generation/cgi-bin/gen_rosinstall.py
@@ -21,27 +21,35 @@ def main():
     for k in keys:
         if not k in form.keys():
             print 'Missing parameters: %s'%k
-            return
+            return -1
     if form['rosdistro'].value not in ['boxturtle', 'cturtle', 'diamondback', 'unstable', 'electric', 'fuerte']:
          # needs to send httperror instead
          print 'invalid rosdistro parameter'
-         return
+         return -1
     if form['overlay'].value not in ['yes', 'no']:
          # needs to send httperror instead
          print 'invalid overlay parameter'
-         return
+         return -1
     p = re.compile('\A[A-Za-z]+[\w\-]*\Z')
     if not bool(p.match(form['variant'].value)):
          # needs to send httperror instead
          print 'invalid variant parameter'
-         return
+         return -1
 
     command = 'export ROS_HOME=/tmp && export ROS_PACKAGE_PATH="/home/willow/ros_release:/opt/ros/cturtle/stacks" && export ROS_ROOT="/opt/ros/cturtle/ros" && export PATH="/opt/ros/cturtle/ros/bin:$PATH" && export PYTHONPATH="/opt/ros/cturtle/ros/core/roslib/src" && rosrun job_generation generate_rosinstall.py --rosdistro %s --variant %s --overlay %s --database /home/willow/rosinstall.db'%(form['rosdistro'].value, form['variant'].value, form['overlay'].value)
 
 
-    res, err = subprocess.Popen(['bash', '-c', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    print '%s %s'%(str(res), str(err))    
-
+    helper = subprocess.Popen(['bash', '-c', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res, err = helper.communicate()
+    if helper.returncode != 0:
+        print '%s'%str(err)
+        return -1
+    else:
+        print '%s'%str(res)
+        return 0
+    
 
 if __name__ == '__main__':
-    main()
+    res = main()
+    sys.exit( res )
+
