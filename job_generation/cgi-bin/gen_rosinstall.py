71be87e509686f40d73655517f2d08be3ef3e09c
Wim Meeussen <wim@willowgarage.com>
2011-02-01 17:55:46 +0000

    sys exit string on failure

--- job_generation/cgi-bin/gen_rosinstall.py
+++ job_generation/cgi-bin/gen_rosinstall.py
@@ -20,21 +20,17 @@ def main():
     keys = ['rosdistro', 'variant', 'overlay']
     for k in keys:
         if not k in form.keys():
-            print 'Missing parameters: %s'%k
-            return -1
+            return 'Missing parameters: %s'%k
     if form['rosdistro'].value not in ['boxturtle', 'cturtle', 'diamondback', 'unstable', 'electric', 'fuerte']:
          # needs to send httperror instead
-         print 'invalid rosdistro parameter'
-         return -1
+         return 'invalid rosdistro parameter'
     if form['overlay'].value not in ['yes', 'no']:
          # needs to send httperror instead
-         print 'invalid overlay parameter'
-         return -1
+         return 'invalid overlay parameter'
     p = re.compile('\A[A-Za-z]+[\w\-]*\Z')
     if not bool(p.match(form['variant'].value)):
          # needs to send httperror instead
-         print 'invalid variant parameter'
-         return -1
+         return 'invalid variant parameter'
 
     command = 'export ROS_HOME=/tmp && export ROS_PACKAGE_PATH="/home/willow/ros_release:/opt/ros/cturtle/stacks" && export ROS_ROOT="/opt/ros/cturtle/ros" && export PATH="/opt/ros/cturtle/ros/bin:$PATH" && export PYTHONPATH="/opt/ros/cturtle/ros/core/roslib/src" && rosrun job_generation generate_rosinstall.py --rosdistro %s --variant %s --overlay %s --database /home/willow/rosinstall.db'%(form['rosdistro'].value, form['variant'].value, form['overlay'].value)
 
@@ -42,14 +38,12 @@ def main():
     helper = subprocess.Popen(['bash', '-c', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     res, err = helper.communicate()
     if helper.returncode != 0:
-        print '%s'%str(err)
-        return -1
+        return '%s'%str(err)
     else:
         print '%s'%str(res)
         return 0
     
 
 if __name__ == '__main__':
-    res = main()
-    sys.exit( res )
+    sys.exit(main())
 
