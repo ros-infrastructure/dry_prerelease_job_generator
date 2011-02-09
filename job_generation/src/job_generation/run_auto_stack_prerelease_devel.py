b624e29880500e50e1f9771443ee392441345c8f
Tully Foote <tfoote@willowgarage.com>
2011-02-09 01:26:26 +0000

    more debuggin

--- job_generation/src/job_generation/run_auto_stack_prerelease_devel.py
+++ job_generation/src/job_generation/run_auto_stack_prerelease_devel.py
@@ -49,6 +49,7 @@ def main():
         f.write(rosinstall)
     call('whoami', env)
     call('ls -lR ~/.subversion', env)
+    call('svn co https://bosch-ros-pkg.svn.sourceforge.net/svnroot/bosch-ros-pkg/trunk/stacks/bosch_drivers /tmp/bd', env)
     call('rosinstall %s /opt/ros/%s %s'%(STACK_DIR, options.rosdistro, rosinstall_file), env,
          'Install the stacks to test from source.')
 
