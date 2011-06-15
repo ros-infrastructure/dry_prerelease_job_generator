8b670aa84d1648ece5df9d4b3d985d339d48629e
Wim Meeussen <wim@willowgarage.com>
2011-06-15 16:33:54 +0000

    add debug print

--- job_generation/src/job_generation/run_auto_stack_devel.py
+++ job_generation/src/job_generation/run_auto_stack_devel.py
@@ -68,6 +68,7 @@ def main():
 
     # global except
     except Exception, ex:
+        print "Global exception caught. Generating email"
         generate_email("%s. Check the console output for test failure details."%ex, env)
         traceback.print_exc(file=sys.stdout)
         raise ex
