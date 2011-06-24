1689ad404ebd19620c31764ea70f9adc68f1eb51
Ethan Rublee <erublee+github@willowgarage.com>
2011-06-24 23:00:50 +0000

    check_call

--- rosdeb/scripts/build_sourcedebs.py
+++ rosdeb/scripts/build_sourcedebs.py
@@ -172,7 +172,7 @@ def do_deb_build(distro_name, stack_name, stack_version, os_platform, arch, stag
     print "Actually trying to build %s-%s..."%(stack_name, stack_version)
 
     subprocess.check_call(['sudo','apt-get','install','git-core', 'git-buildpackage','-y'])
-    subprocess.check_call("git clone %(stack_name)s %(staging_dir)s/co"%locals())
+    subprocess.check_call(["git", "clone", "%(stack_name)s"%locals(),"%(staging_dir)s/co"%locals()])
     subprocess.check_call(["/bin/sh","-c", "'cd %(staging_dir)s/co && git branch upsteam origin/upstream && git-buildpackage -S'"%locals()])
 
     distro_tgz = os.path.join('/var/cache/pbuilder', "%s-%s.tgz"%(os_platform, arch))
