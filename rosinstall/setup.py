dc7d2355a64c55e5cef83825aad3e25a03aeef1c
Ken Conley <kwc@kwc.org>
2011-09-01 21:38:52 +0000

    rosinstall: merging in new rosco tool with new roslocate-like syntax

--- rosinstall/setup.py
+++ rosinstall/setup.py
@@ -1,13 +1,11 @@
-
 from setuptools import setup
 
-
 setup(name='rosinstall',
-      version= '0.5.20',
+      version= '0.5.21',
       packages=['rosinstall'],
       package_dir = {'':'src'},
       install_requires=['vcstools'],
-      scripts = ["scripts/rosinstall", "scripts/roslocate", "scripts/rosws"],
+      scripts = ["scripts/rosinstall", "scripts/roslocate", "scripts/rosws", "scripts/rosco"],
       author = "Tully Foote", 
       author_email = "tfoote@willowgarage.com",
       url = "http://www.ros.org/wiki/rosinstall",
--- rosinstall/setup.sh
+++ rosinstall/setup.sh
@@ -1,1 +1,10 @@
-export PYTHONPATH=`rospack find rosinstall`/src:$PYTHONPATH
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+export PYTHONPATH=$SCRIPT_PATH/src:$PYTHONPATH
