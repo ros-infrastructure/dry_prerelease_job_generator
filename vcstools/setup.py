96a24c4fb339df8623db16563a126ab6c2134266
Ken Conley <kwc@kwc.org>
2011-09-01 17:25:03 +0000

    pushed 0.0.2

--- vcstools/setup.py
+++ vcstools/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='vcstools',
-      version= '0.0.1',
+      version= '0.0.2',
       packages=['vcstools'],
       package_dir = {'':'src'},
       scripts = [],
@@ -9,11 +9,11 @@ setup(name='vcstools',
       author_email = "tfoote@willowgarage.com",
       url = "http://www.ros.org/wiki/vcstools",
       download_url = "http://pr.willowgarage.com/downloads/vcstools/", 
-      keywords = ["scm", "vcs"],
+      keywords = ["scm","vcs","git", "svn","hg","bzr"],
       classifiers = [
         "Programming Language :: Python", 
         "License :: OSI Approved :: BSD License" ],
-      description = "VCS library for svn, git, hg, and bzr", 
+      description = "VCS/SCM source control library for svn, git, hg, and bzr", 
       long_description = """\
 Library for managing source code trees from multiple version control systems. 
 Current supports svn, git, hg, and bzr.
