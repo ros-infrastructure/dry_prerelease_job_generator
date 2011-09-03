87f14d33672a3ad00703475128c49de9004cdd3b
Ken Conley <kwc@kwc.org>
2011-09-03 20:02:48 +0000

    removing viewcode, which isn't available on doc server

--- vcstools/doc/conf.py
+++ vcstools/doc/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.doctest', 'sphinx.ext.coverage', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.doctest', 'sphinx.ext.coverage', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
