1cff5ecc102ea000af18edc0a2b5ebdd392e49b4
Ken Conley <kwc@kwc.org>
2012-01-19 23:22:31 +0000

    removing pkg-config hack

--- rosdeb/resources/source_deb/setup_deb.sh
+++ rosdeb/resources/source_deb/setup_deb.sh
@@ -1,5 +1,3 @@
 . /opt/ros/${ROS_DISTRO_NAME}/setup.sh
 export ROS_HOME=/tmp/.ros
-#TODO:FIXME:REMOVE
-sudo apt-get install pkg-config
 export ROS_PACKAGE_PATH=${ROS_DESTDIR}/opt/ros/${ROS_DISTRO_NAME}/stacks:${ROS_PACKAGE_PATH}
