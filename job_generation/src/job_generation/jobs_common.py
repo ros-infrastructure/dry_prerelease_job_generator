dab569e01d539194b46ec9ca9b5f86a02157549b
Wim Meeussen <wim@willowgarage.com>
2012-01-31 21:35:11 +0000

    use new hudson helper for fuerte

--- job_generation/src/job_generation/jobs_common.py
+++ job_generation/src/job_generation/jobs_common.py
@@ -31,7 +31,7 @@ export ROS_PACKAGE_PATH=\$INSTALL_DIR/ros_release:\$ROS_PACKAGE_PATH
 mkdir -p \$INSTALL_DIR
 cd \$INSTALL_DIR
 
-wget  --no-check-certificate http://code.ros.org/svn/ros/installers/trunk/hudson/hudson_helper 
+wget  --no-check-certificate http://code.ros.org/svn/ros/installers/branches/fuerte/hudson/hudson_helper 
 chmod +x  hudson_helper
 svn co https://code.ros.org/svn/ros/stacks/ros_release/branches/fuerte ros_release
 """
