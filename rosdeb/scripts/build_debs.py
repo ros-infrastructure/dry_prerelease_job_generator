6b47814a1c376bc15174541721d650cf77b40efc
Tully Foote <tfoote@willowgarage.com>
2012-04-25 23:14:03 +0000

    fixing more incorrect usages of platforms()

--- rosdeb/scripts/build_debs.py
+++ rosdeb/scripts/build_debs.py
@@ -745,8 +745,11 @@ def build_debs_main():
         staging_dir = tempfile.mkdtemp()
 
     try:
-        if os_platform not in rosdeb.platforms():
-            raise BuildFailure("[%s] is not a known platform.\nSupported platforms are: %s"%(os_platform, ' '.join(rosdeb.platforms())))
+        if distro_name not in rosdeb.targets.os_platform[distro_name]:
+            raise BuildFailure("[%s] is not a known rosdistro.\nValid rosdistros are: %s" % (distro_name, ' '.join(os.targest.os_platform.keys())))
+        target_platforms = rosdeb.targets.os_platform[distro_name]
+        if os_platform not in target_platforms:
+            raise BuildFailure("[%s] is not a known platform.\nSupported platforms are: %s" % (os_platform, ' '.join(target_platforms)))
 
         if not os.path.exists(staging_dir):
             debug("creating staging dir: %s"%(staging_dir))
