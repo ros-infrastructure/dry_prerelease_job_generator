a336c3506234ecc82a8f06fabf10834bdb308fa7
Tully Foote <tfoote@willowgarage.com>
2012-09-02 08:31:52 +0000

    viewing deb name

--- rosdeb/scripts/list_missing.py
+++ rosdeb/scripts/list_missing.py
@@ -160,11 +160,12 @@ def compute_missing_depends(stack_name, distro, os_platform, arch, repo=SHADOW_R
     deps = set([(sn, sv) for (sn, sv) in deps if not sn == stack_name])
     for sn, sv in deps:
         deb_name = "ros-%s-%s"%(distro, debianize_name(sn))
+        
         #deb_version = '[0-9.]*-[st][0-9]+~[a-z]+'
         # be very promisucous
         deb_version = '.*'
         if not deb_in_repo(repo, deb_name, deb_version, os_platform, arch, use_regex=True):
-            missing_deps.add(sn)
+            missing_deps.add(deb_name)
 
     return missing_deps
 
