71e2ecfbc3f3a160efd8580991d5e96795712d82
Tully Foote <tfoote@willowgarage.com>
2012-09-03 03:20:35 +0000

    print distro version

--- rosdeb/scripts/list_missing.py
+++ rosdeb/scripts/list_missing.py
@@ -162,7 +162,7 @@ def compute_missing_depends(stack_name, distro, os_platform, arch, repo=SHADOW_R
         deb_name = "ros-%s-%s"%(distro.release_name, debianize_name(sn))
         
         deb_version = '[0-9.]*-[st][0-9]+~[a-z]+|[0-9.]*-[0-9a-z]+-[0-9]+-[0-9]+-\+0000'
-        print "deb_version", deb_version
+        print "deb_version", deb_version, 'local_version', sv
         if not deb_in_repo(repo, deb_name, deb_version, os_platform, arch, use_regex=True):
             print "missing dependency", deb_name
             missing_deps.add(deb_name)
