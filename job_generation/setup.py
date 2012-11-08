dc28969dd4034d5025ec68dd87de77c5e727b167
Wim Meeussen <wim@willowgarage.com>
2012-11-08 18:59:18 +0000

    new version

--- job_generation/setup.py
+++ job_generation/setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup
 
 setup(name='ros-groovy-job-generation',
-      version= '0.2.2',
+      version= '0.2.3',
       install_requires=['python-jenkins', 'rospkg', 'rosdep'],
       packages=['job_generation_groovy'],
       package_dir = {'':'src'},
       scripts = ['scripts/generate_groovy_devel.py',
                  'scripts/generate_groovy_prerelease.py',
                  ],
-      author = "Wim Meeussen", 
+      author = "Wim Meeussen",
       author_email = "wim@hidof.com",
       url = "http://www.ros.org/wiki/job_generation",
-      download_url = "http://pr.willowgarage.com/downloads/", 
+      download_url = "http://pr.willowgarage.com/downloads/",
       keywords = ["ROS"],
       classifiers = [
-        "Programming Language :: Python", 
+        "Programming Language :: Python",
         "License :: OSI Approved :: BSD License" ],
-      description = "build farm job generation for dry jobs", 
+      description = "build farm job generation for dry jobs",
       long_description = """\
 build farm job generation for dry jobs
 """,
