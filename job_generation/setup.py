82bf97316cdb859a1fa8c5dd921f813e1039e810
Wim Meeussen <wim@willowgarage.com>
2012-11-08 19:35:33 +0000

    update

--- job_generation/setup.py
+++ job_generation/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ros-groovy-job-generation',
-      version= '0.2.3',
+      version= '0.2.4',
       install_requires=['python-jenkins', 'rospkg', 'rosdep'],
       packages=['job_generation_groovy'],
       package_dir = {'':'src'},
